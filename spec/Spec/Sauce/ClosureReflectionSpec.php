a96042419ff13e45e2b9d30934563b02cd6f5833
Ilya S <public.ilya.s@gmail.com>
2014-04-16 16:57:11 +0700

    Implement Sauce/ClosureReflection::withContext

--- spec/Spec/Sauce/ClosureReflectionSpec.php
+++ spec/Spec/Sauce/ClosureReflectionSpec.php
@@ -5,12 +5,25 @@ use Prophecy\Argument;
 
 class ClosureReflectionSpec extends ObjectBehavior {
 
+    function let()
+    {
+        $this->beConstructedWith(function()
+        {
+            return $this->foo;
+        });
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Sauce\ClosureReflection');
     }
 
+    function it_invokes_a_closure_with_the_given_context()
+    {
+        $context = (object) ['foo' => 'Hello, world!'];
 
+        $this->withContext($context)->shouldReturn('Hello, world!');
+    }
 
 }
 
--- src/Sauce/ClosureReflection.php
+++ src/Sauce/ClosureReflection.php
@@ -2,7 +2,36 @@
 
 class ClosureReflection {
 
+    /**
+     * The closure we work with
+     *
+     * @var \Closure
+     */
+    protected $closure;
 
+    /**
+     * The constructor
+     *
+     * @param \Closure $closure
+     * @return ClosureReflection
+     */
+    public function __construct(\Closure $closure)
+    {
+        $this->closure = $closure;
+    }
+
+    /**
+     * Execute the closure with a given context
+     *
+     * @param mixed $context
+     * @return mixed
+     */
+    public function withContext($context)
+    {
+        $bound = $this->closure->bindTo($context);
+
+        return $bound();
+    }
 
 }
 
