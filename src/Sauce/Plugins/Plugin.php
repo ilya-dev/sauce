c5c9890071de6f349aab3594535b447f5df18374
Ilya S <public.ilya.s@gmail.com>
2014-04-16 20:10:44 +0700

    add abstract Sauce/Plugins/Plugin

---
+++ src/Sauce/Plugins/Plugin.php
@@ -0,0 +1,35 @@
+<?php namespace Sauce\Plugins;
+
+abstract class Plugin {
+
+    /**
+     * The plugin settings
+     *
+     * @var array
+     */
+    protected $settings = [];
+
+    /**
+     * Run a plugin
+     *
+     * @param string $from
+     * @param string $to
+     * @return mixed
+     */
+    abstract protected function run($from, $to);
+
+    /**
+     * Gets called when the object is called as a function
+     *
+     * @param array $settings
+     * @return mixed
+     */
+    public function __invoke(array $settings = [])
+    {
+        $this->settings = $settings;
+
+        return $this;
+    }
+
+}
+
