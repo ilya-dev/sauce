772c08e65159cb5638d5a9d9c3217bc34be946cd
Ilya S <public.ilya.s@gmail.com>
2014-04-16 19:01:32 +0700

    add src/functions.php

---
+++ src/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+if ( ! function_exists('task'))
+{
+    function task()
+    {
+        $arguments = func_get_args();
+
+        // call_user_func_array(['Sauce\Task', 'register'], $arguments) ?
+    }
+}
+
