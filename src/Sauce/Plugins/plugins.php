acea4f443eeff77982411e6b665a04f55bcdcb14
Ilya S <public.ilya.s@gmail.com>
2014-04-17 19:13:13 +0700

    add a config file

---
+++ src/Sauce/Plugins/plugins.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'copy'       =>   'Sauce\Plugins\CopyPlugin',
+    'move'       =>   'Sauce\Plugins\MovePlugin',
+    'concat'     =>   'Sauce\Plugins\ConcatPlugin',
+
+];
+
