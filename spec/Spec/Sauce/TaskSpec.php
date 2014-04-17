937c631237e8941a230c259436093e637cf9f764
Ilya S <public.ilya.s@gmail.com>
2014-04-17 10:41:52 +0700

    add Task::__construct

--- spec/Spec/Sauce/TaskSpec.php
+++ spec/Spec/Sauce/TaskSpec.php
@@ -5,6 +5,11 @@ use Prophecy\Argument;
 
 class TaskSpec extends ObjectBehavior {
 
+    function let()
+    {
+        $this->beConstructedWith('default', ['minify_css', 'minify_js']);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Sauce\Task');
--- src/Sauce/Task.php
+++ src/Sauce/Task.php
@@ -2,7 +2,33 @@
 
 class Task {
 
+    /**
+     * The task name
+     *
+     * @var string
+     */
+    protected $name;
 
+    /**
+     * The dependenc(y/ies)
+     *
+     * @var array|\Closure
+     */
+    protected $dependencies;
+
+    /**
+     * The constructor
+     *
+     * @param string $name
+     * @param array|\Closure $dependencies
+     * @return Task
+     */
+    public function __construct($name, $dependencies)
+    {
+        $this->name = $name;
+
+        $this->dependencies = $dependencies;
+    }
 
 }
 
