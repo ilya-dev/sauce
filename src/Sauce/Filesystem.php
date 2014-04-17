d689c701e59d58771fb92b5ba9b480b58039bb90
Ilya S <public.ilya.s@gmail.com>
2014-04-17 21:27:10 +0700

    Inject Symfony\Finder to Sauce\Filesystem

--- src/Sauce/Filesystem.php
+++ src/Sauce/Filesystem.php
@@ -1,8 +1,26 @@
 <?php namespace Sauce;
 
+use Symfony\Component\Finder\Finder;
+
 class Filesystem {
 
+    /**
+     * The Finder instance
+     *
+     * @var \Symfony\Component\Finder\Finder
+     */
+    protected $finder;
 
+    /**
+     * The constructor
+     *
+     * @param \Symfony\Component\Finder\Finder|null $finder
+     * @return Filesystem
+     */
+    public function __construct(Finder $finder = null)
+    {
+        $this->finder = $finder ?: new Finder;
+    }
 
 }
 
